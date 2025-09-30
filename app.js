f9eec0ebdbf2eadfd4041f6dced01537f696a308
Edris Arjmandrad <itarjmandrad@gamil.com>
2025-10-01 00:26:24 +0330

    know which remote

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+//router
+app.use("/api", route)
+
 // Example route
 app.get("/", (req, res) => {
   res.send("NOW we'er on");
